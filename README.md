ab52366d655728c788710c980365b6de2741aed6
g-susvs <jesus.savs19@gmail.com>
2022-11-10 17:56:25 -0500

    test

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ Una vez configurado ejecutar el proyecto.
 ```sh
 yarn dev
 ```
+
+[Jest config](https://gist.github.com/Klerith/ca7e57fae3c9ab92ad08baadc6c26177)
---
+++ test/todoReducer.test.js
@@ -0,0 +1,72 @@
+import { todoReducer } from "../src/todoReducer"
+
+describe('Pruebas en todoReducer', () => {
+    
+    const initialState = [
+        { id: 1, description: 'tarea 1', done: false },
+        { id: 2, description: 'tarea 2', done: false },
+    ];
+
+    test('debe regresar el valor inicial', () => {
+
+        const todos = todoReducer(initialState,);
+        expect(initialState).toStrictEqual(todos);
+
+    });
+
+    test('debe agregar el todo', () => {
+
+        const action = {
+            type: 'ADD',
+            payload: {
+                id: 3,
+                description: 'tarea nueva',
+                done: true
+            }
+        }
+
+        const todos = todoReducer(initialState, action);
+        expect(todos.includes(action.payload)).toBeTruthy();
+        expect(todos).toContain(action.payload);
+        
+    });
+
+    test('debe eliminiar el todo', () => { 
+
+        const action = {
+            type:'DELETE',
+            payload: 1
+        }
+        
+        const todos = todoReducer(initialState, action);
+        expect(todos.length).toBe(1);
+        expect(todos).toHaveLength(1);
+     })
+    test('debe hacer el togle del todo', () => { 
+
+        const action = {
+            type:'TOGGLE',
+            payload: 1
+        }
+        
+        const todos = todoReducer(initialState, action);
+        expect(todos[0].done).toBeTruthy();
+
+    })
+
+    test('debe editar un todo', () => { 
+        const description = "Hacer testing";
+
+        const action = {
+            type:'EDIT',
+            payload: {
+                id: 1,
+                description
+            }
+        }
+        
+        const todos = todoReducer(initialState, action);
+        expect(todos[0].description).toBe(description);
+
+    })
+})
